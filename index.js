1abd5fa429606f264ae6d4c7da62f8c68d56b99e
TheRexE <104687647+TheRexE@users.noreply.github.com>
2022-04-30 16:33:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const {
+  MessageEmbed
+} = require("discord.js");
+const DiscordMusicBot = require("./structures/DiscordMusicBot");
+const client = new DiscordMusicBot();
+
+client.build();
+
+module.exports = client;
